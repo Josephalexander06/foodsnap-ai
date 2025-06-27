ff0b51116ad0da8072ed1eb1041f2817ddb3d799
Joseph Alexander <josephalexander283@gmail.com>
2025-06-27 11:20:26 +0530

    force update

--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
 import requests, os
 import sqlite3
 import requests , re
-
+#sam
 API_KEY ='L8J1Jo3KTEr03j6nVb5x8C8GVeTus9l4RvmRnnPX'
 
 dataset_folder = ("dataset/")
@@ -21,60 +21,6 @@ cur.execute('''CREATE TABLE IF NOT EXISTS nutrition (
     serving_size REAL
 )''')
 
-# def get_nutrition(food):
-#     print(food)
-#     try:
-#         # Search food
-#         search_url = "https://api.nal.usda.gov/fdc/v1/foods/search"
-#         search_params = {
-#             "query": food,
-#             "api_key": API_KEY,
-#             "pageSize": 1
-#         }
-
-#         search_res = requests.get(search_url, params=search_params)
-#         search_res.raise_for_status()
-#         search_data = search_res.json()
-
-#         if not search_data.get("foods"):
-#             return None
-
-#         food_data = search_data["foods"][0]
-#         fdc_id = food_data["fdcId"]
-#         print('id',fdc_id)
-#         description = food_data.get("description", food)
-
-#         # Get nutrition detail
-#         detail_url = f"https://api.nal.usda.gov/fdc/v1/food/{fdc_id}"
-#         detail_res = requests.get(detail_url, params={"api_key": API_KEY})
-#         detail_data = detail_res.json()
-
-#         nutrient_map = {
-#             1008: 0,  # Energy (kcal)
-#             1003: 0,  # Protein
-#             1004: 0,  # Fat
-#             1005: 0   # Carbs
-#         }
-#         for nutrient in detail_data.get("foodNutrients", []):
-#             nid = nutrient.get("nutrient", {}).get("id")
-#             val = nutrient.get("amount")
-#             if nid in nutrient_map and val is not None:
-#                 nutrient_map[nid] = val
-
-#         calories = nutrient_map[1008]
-#         protein = nutrient_map[1003]
-#         fat = nutrient_map[1004]
-#         carbs = nutrient_map[1005]
-#         print(calories,protein,fat,carbs)
-
-#         return calories, protein, fat, carbs, description
-    
-#     except requests.exceptions.RequestException as e:
-#         print(f"[Network Error] {e}")
-#         return None
-#     except Exception as e:
-#         print(f"[Other Error] {e}")
-#         return None
 
 def clean_food_name(name):
     # Replace underscores, remove digits, strip extras
